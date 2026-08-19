506eb949c936063b79768532f0e09909e36f19ed
SHEILA-DIAZ <sheila.diaz@tecsup.edu.pe>
2026-08-19 09:18:25 -0500

    Agregar vista de bienvenida con icono Swift

--- Laboratorio00/ContentView.swift
+++ Laboratorio00/ContentView.swift
@@ -1,19 +1,20 @@
-//
-//  ContentView.swift
-//  Laboratorio00
-//
-//  Created by Tecsup on 19/08/26.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(spacing: 20) {
+            Image(systemName: "swift")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+                .font(.system(size: 60))
+            
+            Text("¡Hola, iOS Avanzado!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Mi primer commit desde Xcode")
+                .font(.subheadline)
+                .foregroundColor(.gray)
         }
         .padding()
     }
